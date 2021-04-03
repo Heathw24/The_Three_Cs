c34a866e7be28665c8129d50f7c471f3b177f72b
lundyc0917 <lundyc0917@gmail.com>
2021-04-03 14:07:47 -0400

    app.js function fixes

--- the3cs/src/components/App.js
+++ the3cs/src/components/App.js
@@ -4,62 +4,63 @@ import '../styling/App.css';
 
 // import 'react-calendar/dist/Calendar.css';
 
-function App(props) {
-  const [value, onChange] = useState(new Date());
+// function App(props) {
+//   const [value, onChange] = useState(new Date());
+//   return (
+//     <div>
+//       <h1>Welcome {props.user.username} </h1>
+//        <Calendar
+//         onChange={onChange}
+//         value={value}
+//       />
+//     </div>
+//   );
+// }
+
+function App() {
+
+  function showContent(){
+    // TODO: add onClick to display date information
+    return 'Hello';
+  }
+
+  function tileContent({ date, view }) {
+    return showContent();
+      }
+
+      const [value, setValue] = useState(new Date());
+  const [textshown, settextshown] = useState(false);
+  function opentextbox(date) {
+    settextshown(true)
+  }
+  const [recipeSearch, setRecipeSearch] = useState("");
+  function onChange(nextValue){
+    setValue(nextValue);
+  }
+
+  const handleInputChange = event => {
+    const { value } = event.target;
+    setRecipeSearch(value);
+  }; 
+  const handleFormSubmit = event => {
+  };
+  
   return (
     <div>
-      <h1>Welcome {props.user.username} </h1>
-       <Calendar
+      <Calendar
+        onclickday= {opentextbox}
         onChange={onChange}
         value={value}
+        tileContent={tileContent}
       />
+      {textshown&&<input 
+        name="Enter $"
+        value={recipeSearch}
+        onChange={handleInputChange}
+        placeholder="Daily income">
+        </input>}
     </div>
   );
 }
-
-// function App() {
-//   function tileContent({ date, view }) {
-//     // Add class to tiles in month view only
-//     if (view === 'month') {
-//       // Check if a date React-Calendar wants to check is on the list of dates to add class to
-//       if (datesToAddContentTo.find(dDate => isSameDay(dDate, date))) {
-//         return 'My content';
-//       }
-//     }
-//   }
-//   }
-//   function MyApp() {
-//     const [value, setValue] = useState(new Date());
-//     const [textshown, settextshown] = useState(false);
-//     function opentextbox(date) {
-//      settextshown(true)
-//     }
-//     const [recipeSearch, setRecipeSearch] = useState("");
-//   const handleInputChange = event => {
-//     const { value } = event.target;
-//   setRecipeSearch(value);
-// }; 
-// const handleFormSubmit = event => {
-// };
-// <Input
-// name="Enter $"
-// value={recipeSearch}
-// onChange={handleInputChange}
-// placeholder="Daily income"
-// />
-//     return (
-//       <div>
-//       <Calendar
-//      onclickday= {opentextbox}
-//         onChange={onChange}
-//         value={date}
-//         tileContent={tileContent}
-      
-//     />
-//     {textshown&&<input></input>}
-    
-//     </div>
-//     );
-//   }
 
 export default App;
