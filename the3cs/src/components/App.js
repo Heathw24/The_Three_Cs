043d1f317c1e2483477f8309fc5d4489d44a19e6
Austin Cochran <cochran7004@gmail.com>
2021-04-03 12:23:18 -0400

    added calender click event listeners

--- the3cs/src/components/App.js
+++ the3cs/src/components/App.js
@@ -6,16 +6,56 @@ import '../styling/App.css';
 
 
 function App() {
-  const [value, onChange] = useState(new Date());
+  function tileContent({ date, view }) {
+    // Add class to tiles in month view only
+    if (view === 'month') {
+      // Check if a date React-Calendar wants to check is on the list of dates to add class to
+      if (datesToAddContentTo.find(dDate => isSameDay(dDate, date))) {
+        return 'My content';
+      }
+    }
+  }
+  }
+  function MyApp() {
+    const [value, setValue] = useState(new Date());
+    const [textshown, settextshown] = useState(false);
+    function opentextbox(date) {
+     settextshown(true)
+    }
+    const [recipeSearch, setRecipeSearch] = useState("");
 
-  return (
-    <div>
-       <Calendar
+  const handleInputChange = event => {
+    const { value } = event.target;
+  setRecipeSearch(value);
+}; 
+const handleFormSubmit = event => {
+};
+<Input
+name="Enter $"
+value={recipeSearch}
+onChange={handleInputChange}
+placeholder="Daily income"
+/>
+  
+
+
+
+
+
+
+    return (
+      <>
+      <Calendar
+     onclickday= {opentextbox}
         onChange={onChange}
-        value={value}
-      />
-    </div>
-  );
-}
+        value={date}
+        tileContent={tileContent}
+      
+    />
+    {textshown&&<input></input>}
+    
+    </>
+    );
+  }
 
 export default App;
